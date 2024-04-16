2b07cff6e8f0d690bc66da33d0b88aa75438aaa3
minhit6505 <152968883+minhit6505@users.noreply.github.com>
2024-04-16 09:57:16 +0700

    Update order.js

--- src/apps/controllers/apis/order.js
+++ src/apps/controllers/apis/order.js
@@ -17,6 +17,7 @@ exports.order = async (req, res)=>{
         items: body.items,
     };
     await OrderModel(order).save();
+    /*
     const idsPrd = body.items.map((item)=>item.prd_id);
     const products = await ProductModel.find({_id: {$in: idsPrd}}).lean();
     let items = [];
@@ -44,7 +45,7 @@ exports.order = async (req, res)=>{
         subject: "Xác nhận đơn hàng từ Vietpro Store",
         html,
     });
-
+    */
     res 
         .status(201).json({
             status: "success",
