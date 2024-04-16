0b10f44e7d6d809265c3e95a4ee6ed8f5b53ab6a
minhit6505 <152968883+minhit6505@users.noreply.github.com>
2024-04-16 11:47:10 +0700

    Update order.js

--- src/apps/controllers/apis/order.js
+++ src/apps/controllers/apis/order.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const path = require("path");
 exports.order = async (req, res)=>{
  
-    const body = req.body;
+    const body = req.query;
     let totalPrice = 0;
     totalPrice = body.items.reduce((total, item)=>total + item.qty*item.price, 0);
     const order = {
