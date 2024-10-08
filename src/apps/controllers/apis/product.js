f29874c791309b44fa2a01b8aecbfd67b53597c8
minhit6505 <minhit6505@gmail.com>
2024-10-08 20:20:27 +0700

    commited

--- src/apps/controllers/apis/product.js
+++ src/apps/controllers/apis/product.js
@@ -1,102 +1,100 @@
 const ProductModel = require("../../models/product");
 const CommentModel = require("../../models/comment");
 const pagination = require("../../../libs/pagination");
-exports.index = async (req, res)=>{
+exports.index = async (req, res) => {
+  try {
     const query = {};
     // Text Search
     const name = req.query.name || "";
-    if(name){
-        query.$text = {$search: name} 
+    if (name) {
+      query.$text = { $search: name };
     }
     // Logic Query
     // query.is_featured = req.query.is_featured || false;
     // query.is_stock = req.query.is_stock || true;
-    if(req.query.is_featured){
-        query.is_featured = req.query.is_featured;
+    if (req.query.is_featured) {
+      query.is_featured = req.query.is_featured;
     }
-    if(req.query.is_stock){
-        query.is_stock = req.query.is_stock;
+    if (req.query.is_stock) {
+      query.is_stock = req.query.is_stock;
     }
 
     // Pagination
     const limit = parseInt(req.query.limit) || 10;
     const page = parseInt(req.query.page) || 1;
-    const skip = page*limit - limit;
+    const skip = page * limit - limit;
     const products = await ProductModel.find(query)
-        .sort({_id: -1})
-        .skip(skip)
-        .limit(limit)
-    res
-        .status(200)
-        .json({
-            status: "success",
-            filters: {
-                is_featured: query.is_featured,
-                is_stock: query.is_stock,
-                page,
-                limit,
-            },
-            data: {
-                docs: products,
-                pages: await pagination(ProductModel, query, page, limit),
-            },
-            
+      .sort({ _id: -1 })
+      .skip(skip)
+      .limit(limit);
+    res.status(200).json({
+      status: "success",
+      filters: {
+        is_featured: query.is_featured,
+        is_stock: query.is_stock,
+        page,
+        limit,
+      },
+      data: {
+        docs: products,
+        pages: await pagination(ProductModel, query, page, limit),
+      },
     });
-}
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
-exports.show = async (req, res)=>{
-    const {id} = req.params;
+exports.show = async (req, res) => {
+  try {
+    const { id } = req.params;
     const product = await ProductModel.findById(id);
-    res
-        .status(200)
-        .json({
-            status: "success",
-            data: product, 
-        });
-}
-exports.comments = async (req, res)=>{
-    try {
-        const query = {};
+    res.status(200).json({
+      status: "success",
+      data: product,
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+exports.comments = async (req, res) => {
+  try {
+    const query = {};
     query.product_id = req.params.id;
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
-    const skip = page*limit - limit;
+    const skip = page * limit - limit;
     const comments = await CommentModel.find(query)
-        .sort({_id: -1})
-        .skip(skip)
-        .limit(limit);
-    res
-        .status(200)
-        .json({
-            status: "success",
-            data:{
-                docs: comments,
-                pages: await pagination(CommentModel, query, page, limit),
-            },
-            
-        });
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-}
-exports.storeComments = async (req, res)=>{
-    try {
-        const {id} = req.params;
+      .sort({ _id: -1 })
+      .skip(skip)
+      .limit(limit);
+    res.status(200).json({
+      status: "success",
+      data: {
+        docs: comments,
+        pages: await pagination(CommentModel, query, page, limit),
+      },
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+exports.storeComments = async (req, res) => {
+  try {
+    const { id } = req.params;
     const body = req.body;
     const comment = {
-        name: body.name,
-        email: body.email,
-        content: body.content,
-        product_id: id,
-    }
+      name: body.name,
+      email: body.email,
+      content: body.content,
+      product_id: id,
+    };
     await new CommentModel(comment).save();
-    res 
-        .status(201)
-        .json({
-            status: "success",
-            message: "create comment successfully"
-        });
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-}
+    res.status(201).json({
+      status: "success",
+      message: "create comment successfully",
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
