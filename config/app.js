4caf5dec7d46ae4d8ea5d9b34957f22bcff0891d
minhit6505 <minhit6505@gmail.com>
2024-11-06 16:07:11 +0700

    commited

--- config/app.js
+++ config/app.js
@@ -1,8 +1,8 @@
 module.exports = {
-    port: process.env.SERVER_PORT || 8000,
-    prefixApiVersion: process.env.PREFIX_API_VERSION || "/api/v1",
-    jwtAccessKey: process.env.JWT_ACCESS_KEY || "",
-    jwtRefreshKey: process.env.JWT_REFRESH_KEY || "",
-    // useAuthMiddleware: true,
-    useAuthMiddleware: false,
+  port: process.env.SERVER_PORT || 8000,
+  prefixApiVersion: process.env.PREFIX_API_VERSION || "/api/v1",
+  jwtAccessKey: process.env.JWT_ACCESS_KEY || "",
+  jwtRefreshKey: process.env.JWT_REFRESH_KEY || "",
+  useAuthMiddleware: true,
+  // useAuthMiddleware: false,
 };
