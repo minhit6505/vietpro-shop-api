10badb478a17a1dd4d95de519b6e36c4ca409c04
minhit6505 <minhit6505@gmail.com>
2024-11-10 14:18:09 +0700

    commited

--- config/app.js
+++ config/app.js
@@ -3,6 +3,6 @@ module.exports = {
   prefixApiVersion: process.env.PREFIX_API_VERSION || "/api/v1",
   jwtAccessKey: process.env.JWT_ACCESS_KEY || "",
   jwtRefreshKey: process.env.JWT_REFRESH_KEY || "",
-  useAuthMiddleware: true,
-  // useAuthMiddleware: false,
+  // useAuthMiddleware: true,
+  useAuthMiddleware: false,
 };
